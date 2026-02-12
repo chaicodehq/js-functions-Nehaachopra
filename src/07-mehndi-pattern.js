a5fc5daabc86e59533b62c0384b958f64491271a
Neha Chopra <chopraneha211@gmail.com>
2026-02-12 18:51:56 +0530

    Create mutiple recursive functions

--- src/07-mehndi-pattern.js
+++ src/07-mehndi-pattern.js
@@ -54,20 +54,50 @@
  */
 export function repeatChar(char, n) {
   // Your code here
+  if (typeof char !== "string" || char === "" || n <= 0) return ""
+
+  return char + repeatChar(char, n - 1);
 }
 
 export function sumNestedArray(arr) {
   // Your code here
+  if (!Array.isArray(arr) || arr.length <= 0) return 0
+
+  const [first, ...rest] = arr;
+  const firstValue = Array.isArray(first) ? sumNestedArray(first) : typeof first === "number" ? first : 0;
+
+  return firstValue + sumNestedArray(rest)
 }
 
 export function flattenArray(arr) {
   // Your code here
+  if (!Array.isArray(arr) || arr.length <= 0) return []
+
+  const [first, ...rest] = arr;
+  const firstElement = Array.isArray(first) ? flattenArray(first) : [first];
+
+  return [...firstElement, ...flattenArray(rest)];
 }
 
 export function isPalindrome(str) {
   // Your code here
+  if (typeof str !== "string") return false
+  if (str.length <= 1) return true
+  if (str[0].toLowerCase() !== str[str.length - 1].toLowerCase()) return false
+  return isPalindrome(str.slice(1, -1));
 }
 
 export function generatePattern(n) {
   // Your code here
+  if (!Number.isInteger(n) || n <= 0) return []
+
+  function buildUp(k) {
+    if (k=== 0) return [];
+    return [...buildUp(k-1), "*".repeat(k)]
+  }
+  
+  const top = buildUp(n);
+  const bottom = top.slice(0, -1).reverse();
+
+  return [...top, ...bottom];
 }
